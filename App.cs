164e1fef9c15d6a8d72308634f0a88be9d923c3a
mateuszpodeszwa <podinatubie@gmail.com>
2026-04-01 13:56:13 +0100

    Integrate ConsoleHost and DI into App
    Add ConsoleHost and IServiceProvider to App constructor and import controller/DI namespaces. Rename Settings to MySettings, invoke consoleHost.Host<HomePageController>(MySettings) in RunAsync to host the UI controller, and log exceptions via ILogger in the catch block. These changes wire the app to use the ConsoleHost-based controller hosting and improve error reporting.

--- App.cs
+++ App.cs
@@ -2,7 +2,9 @@ using BeautifulClient.Configuration;
 using BeautifulClient.Data.Objects;
 using BeautifulClient.Services.Api;
 using BeautifulClient.UI;
+using BeautifulClient.UI.Controllers;
 using BeautifulClient.Utilities.ErrorHandler;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Spectre.Console;
@@ -13,9 +15,11 @@ public class App(
     ILogger<App> logger,
     IOptions<MySettings> options,
     IOptions<ApiSettings> apiSettings,
-    IApiService apiService)
+    IApiService apiService,
+    IServiceProvider serviceProvider,
+    ConsoleHost consoleHost)
 {
-    private MySettings Settings { get; } = options.Value;
+    private MySettings MySettings { get; } = options.Value;
     private ApiSettings ApiSettings { get; } = apiSettings.Value;   
 
     public async Task RunAsync()
@@ -24,11 +28,12 @@ public class App(
         
         try
         {
-            
+            await consoleHost.Host<HomePageController>(MySettings);
         }
         catch (Exception e)
         {
             Console.WriteLine(e);
+            logger.LogError(e.ToString());
         }
 
         AppActivity("Ending");
