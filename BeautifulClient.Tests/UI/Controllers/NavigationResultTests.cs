6744e8a416003778cc27e111ccdab2f247893226
mateuszpodeszwa <podinatubie@gmail.com>
2026-04-07 16:37:48 +0100

    Add using directive for Data.Records in NavigationResultTests

--- BeautifulClient.Tests/UI/Controllers/NavigationResultTests.cs
+++ BeautifulClient.Tests/UI/Controllers/NavigationResultTests.cs
@@ -1,3 +1,4 @@
+using BeautifulClient.Data.Records;
 using BeautifulClient.UI.Controllers;
 using Xunit;
 
--- UI/Controllers/NavigationResult.cs
+++
@@ -1,9 +0,0 @@
-namespace BeautifulClient.UI.Controllers;
-
-public record NavigationResult(Type? NextRoute, object? Payload = null);
-
-public sealed record NavigationResult<TPayload>(Type? NextRoute, TPayload? Payload = default)
-{
-	public static implicit operator NavigationResult(NavigationResult<TPayload> value)
-		=> new(value.NextRoute, value.Payload);
-}
